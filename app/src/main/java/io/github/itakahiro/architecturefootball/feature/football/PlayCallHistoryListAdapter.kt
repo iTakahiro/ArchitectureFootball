2e6492f801bc0b3407fcb4df9cb1d10bbba83aa1
Takahiro Ishikawa <takahiro.ishikawa21@gmail.com>
2019-12-15 14:21:46 +0900

    ダミーデータからDBへ、データの管理を移行

--- app/src/main/java/io/github/itakahiro/architecturefootball/feature/football/PlayCallHistoryListAdapter.kt
+++ app/src/main/java/io/github/itakahiro/architecturefootball/feature/football/PlayCallHistoryListAdapter.kt
@@ -4,13 +4,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
 import io.github.itakahiro.architecturefootball.R
+import io.github.itakahiro.architecturefootball.model.PlayCall
 
 
-class HistoryListAdapter() : RecyclerView.Adapter<HistoryListAdapter.ViewHolder>() {
-    // TODO: String -> PlayCall
-    private val items = mutableListOf<String>()
+class PlayCallHistoryListAdapter(
+    fragment: FootballFragment,
+    viewModel: FootballViewModel
+) : RecyclerView.Adapter<PlayCallHistoryListAdapter.ViewHolder>() {
+    init {
+        viewModel.playCallHistoryList.observe(fragment, Observer { historyList ->
+            historyList.forEach { playCall ->
+                setItem(playCall)
+            }
+        })
+    }
+    private val items = mutableListOf<PlayCall>()
 
     override fun getItemCount(): Int {
         return items.size
@@ -23,15 +34,16 @@ class HistoryListAdapter() : RecyclerView.Adapter<HistoryListAdapter.ViewHolder>
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.textView.text = items[position]
+        holder.textView.text = items[position].description
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val textView = itemView.findViewById<View>(R.id.textViewHistoryItem) as TextView
     }
 
-    // TODO: String -> PlayCall
-    fun setItem(item: String) {
+    private fun setItem(item: PlayCall) {
+        items.clear()
         items.add(item)
+        notifyDataSetChanged()
     }
 }
