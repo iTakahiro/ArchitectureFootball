32402c158d933441cfea96f2b4f4a66de981fb57
Takahiro Ishikawa <takahiro.ishikawa21@gmail.com>
2020-04-20 10:30:44 +0900

    空のレイアウト・Fragment・ViewModelを追加し、レイアウトファイルとFragmentクラスを結びつけ

---
+++ app/src/main/java/io/github/itakahiro/architecturefootball/feature/football/PlayCallListFragment.kt
@@ -0,0 +1,21 @@
+package io.github.itakahiro.architecturefootball.feature.football
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import io.github.itakahiro.architecturefootball.R
+
+class PlayCallListFragment: Fragment() {
+
+    val viewModel = PlayCallListViewModel()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_play_call_list, container, false)
+    }
+}
---
+++ app/src/main/java/io/github/itakahiro/architecturefootball/feature/football/PlayCallListViewModel.kt
@@ -0,0 +1,3 @@
+package io.github.itakahiro.architecturefootball.feature.football
+
+class PlayCallListViewModel
