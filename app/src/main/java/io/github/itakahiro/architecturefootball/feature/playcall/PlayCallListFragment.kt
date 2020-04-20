94a42f772c6c679c6a6cd5acf4a5d137051df7ed
Takahiro Ishikawa <takahiro.ishikawa21@gmail.com>
2020-04-20 12:11:39 +0900

    一覧画面にて一覧表示を実装

--- app/src/main/java/io/github/itakahiro/architecturefootball/feature/playcall/PlayCallListFragment.kt
+++ app/src/main/java/io/github/itakahiro/architecturefootball/feature/playcall/PlayCallListFragment.kt
@@ -5,12 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import io.github.itakahiro.architecturefootball.R
+import io.github.itakahiro.architecturefootball.feature.football.FootballViewModel
+import io.github.itakahiro.architecturefootball.feature.football.PlayCallHistoryListAdapter
+import kotlinx.android.synthetic.main.fragment_play_call_list.playCallList
 
 class PlayCallListFragment: Fragment() {
 
-    val viewModel =
-        PlayCallListViewModel()
+    private val viewModel = FootballViewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,5 +21,19 @@ class PlayCallListFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_play_call_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapter =
+            PlayCallHistoryListAdapter(
+                this,
+                viewModel
+            )
+        playCallList.adapter = adapter
+        playCallList.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+
+        viewModel.loadPlayCallHistoryList()
     }
 }
