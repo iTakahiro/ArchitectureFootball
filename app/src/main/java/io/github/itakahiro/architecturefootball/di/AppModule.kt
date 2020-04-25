e2de7ac35a1a002b2dd84bd3cd85f72e04311449
Takahiro Ishikawa <takahiro.ishikawa21@gmail.com>
2020-04-25 20:41:47 +0900

    @Providesメソッドを1つのModuleに集約するパターン用のAppModuleを作成

---
+++ app/src/main/java/io/github/itakahiro/architecturefootball/di/AppModule.kt
@@ -0,0 +1,32 @@
+package io.github.itakahiro.architecturefootball.di
+
+import dagger.Module
+
+// 1つのModuleに集約するパターン. 微妙かも. メソッドが引数を持ってしまっているから.
+
+@Module
+class AppModule {
+//
+//    // Dao
+//    @Provides
+//    fun providePlayCallDao(): PlayCallDao {
+//        return App.database.playCallDao()
+//    }
+//
+//    // Repository
+//    @Provides
+//    fun providePlayCallRepository(dao: PlayCallDao): PlayCallRepository {
+//        return PlayCallRepository(dao)
+//    }
+//
+//    // ViewModel
+//    @Provides
+//    fun provideFootballViewModel(repository: PlayCallRepository): FootballViewModel {
+//        return FootballViewModel(repository)
+//    }
+//
+//    @Provides
+//    fun providePlayCallListViewModel(repository: PlayCallRepository): PlayCallListViewModel {
+//        return PlayCallListViewModel(repository)
+//    }
+}
