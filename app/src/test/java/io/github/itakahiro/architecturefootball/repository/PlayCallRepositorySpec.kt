eeb6397e424f6d6f06fc8a43cbbc707ee9321940
Takahiro Ishikawa <takahiro.ishikawa21@gmail.com>
2020-05-24 13:30:25 +0900

    Revert "テスト用ファイルをいくつか作成"
    This reverts commit 069cd1b0a044769ba8bfb769d9e95eb089c74c4c.

--- app/src/test/java/io/github/itakahiro/architecturefootball/repository/PlayCallRepositorySpec.kt
+++
@@ -1,9 +0,0 @@
-package io.github.itakahiro.architecturefootball.repository
-
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.gherkin.Feature
-
-object PlayCallRepositorySpec : Spek({
-    Feature("PlayCallRepository") {
-    }
-})
